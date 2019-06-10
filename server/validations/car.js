f238650611c24781de6f6ed9b585a8ac65c14a9f
t4christ <bakaretemitayo7@gmail.com>
2019-06-10 16:07:23 +0100

    update vehicle as sold

--- server/validations/car.js
+++ server/validations/car.js
@@ -1,4 +1,4 @@
-
+import { cars } from '../datastore';
 
 
 /**
@@ -122,6 +122,31 @@ export class Car {
         req.body.bodytype = bodytype.toLowerCase();
         return next();
     }
+
+/**
+   * Fetch Specific Car On to the application
+   * @param {object} req - The request object
+   * @param {object} res - The response object
+   * @param {function} next - Calls the next function/route handler
+   * @returns {object} JSON representing the failure message.
+   */
+  static findSpecificCarAd(req, res, next) {
+    const { id } = req.params;
+    const value = Number(id);
+    // if (value === NaN || value === 0) {
+    //     return 'Invalid search entry'
+    // }
+    const foundCar = cars.find(car => car.id === value);
+    if (!foundCar) {
+        return res.status(404).json({
+            status: 404,
+            error: 'Car not found'
+        });
+    }
+    req.body.foundCar = foundCar;
+    return next();
+}
+
 
 }
 
