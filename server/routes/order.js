572eb29fbc5ee2a8aa98ba1a7ed9f0712fb8a983
t4christ <bakaretemitayo7@gmail.com>
2019-06-10 15:42:04 +0100

    update vehicle order

--- server/routes/order.js
+++ server/routes/order.js
@@ -3,14 +3,14 @@ import { OrderController } from '../controllers';
 import { Order }  from '../validations';
 import { verifyToken } from '../middlewares/auth'
 
-const { postOrder} = OrderController;
+const { postOrder, editOrderPrice } = OrderController;
 const { postOrderChecker } = Order;
 
 export const orderRouter = express.Router();
 
 
 orderRouter.post('/order', verifyToken, postOrderChecker, postOrder);
-
+orderRouter.patch('/order/:orderId/price', verifyToken, editOrderPrice);
 
 
 
