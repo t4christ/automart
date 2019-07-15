87fcf3fa01ea671fe20d8a3e7ef37f97cc1b041a
t4christ <bakaretemitayo7@gmail.com>
2019-07-15 18:14:53 +0100

    adjust seeders file

--- server/seeders/car.js
+++ server/seeders/car.js
@@ -7,10 +7,10 @@ const ownerOne = Number(process.env.AFFICHE_UNO);
 const ownerTwo = Number(process.env.AFFICHE_TROIS);
 
 const variables = [
-    [ownerOne, 'new', 'unsold', '20000000', 'toyota', 'Tundra', 'car', 'https://res.cloudinary.com/oseun/image/upload/v1559912544/toyota-tundra.jpg'],
-    [ownerTwo, 'new', 'unsold', '30000000', 'toyota', 'Hiace', 'van', 'https://res.cloudinary.com/oseun/image/upload/v1559912583/van.jpg'],
-    [ownerOne, 'used', 'unsold', '35000000', 'bmw', 'Runner', 'car', 'https://res.cloudinary.com/oseun/image/upload/v1559912618/car.jpg'],
-    [ownerOne, 'used', 'unsold', '40000000', 'toyota', 'Runner', 'car', 'https://res.cloudinary.com/oseun/image/upload/v1559912278/toyota-runner.jpg'],
+    [ownerOne, 'new', 'unsold', '20000000', 'toyota', 'Tundra', 'car', 'https://res.cloudinary.com/texplode/image/upload/v1559912544/toyota-tundra.jpg'],
+    [ownerTwo, 'new', 'unsold', '30000000', 'toyota', 'Hiace', 'van', 'https://res.cloudinary.com/texplode/image/upload/v1559912583/van.jpg'],
+    [ownerOne, 'used', 'unsold', '35000000', 'bmw', 'Runner', 'car', 'https://res.cloudinary.com/texplode/image/upload/v1559912618/car.jpg'],
+    [ownerOne, 'used', 'unsold', '40000000', 'toyota', 'Runner', 'car', 'https://res.cloudinary.com/texplode/image/upload/v1559912278/toyota-runner.jpg'],
 ];
 
 const sql = format('INSERT INTO cars (owner, state, status, price, manufacturer, model, bodytype, imageurl) VALUES %L returning id', variables);
