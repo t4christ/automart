bcdad1e09b9da23ba7d202806b7c262d468aeae5
t4christ <bakaretemitayo7@gmail.com>
2019-07-16 09:46:11 +0100

    push to heroku

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -12,7 +12,7 @@ export const createToken = (payload) => {
 
 export const verifyToken = (req, res, next) => {
   // req.headers['authorization'] = `Bearer ${req.headers.authorization}`;
-  let token = req.headers.token ||  req.headers.authorization;
+  let token = req.headers.authorization || req.headers.token;
 
 
   console.log('tokennitre', token);
